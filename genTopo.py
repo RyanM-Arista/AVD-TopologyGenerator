584b8705a1276c285b7599ccba0f2acdef0ac0c0
RyanM-Arista <>
2022-02-25 17:32:28 -0500

    add outputs

--- genTopo.py
+++ genTopo.py
@@ -29,8 +29,10 @@ def find(pattern, path):
 
 
 def buildTopo(name,file):
+  if not os.path.exists('./documentation/diagrams/'):
+    os.makedirs('./documentation/diagrams/')
   sourceFilename = file
-  topotype = (name + ".dot")
+  topotype = ('./documentation/diagrams/' + name + ".dot")
   #print(outputFilename[0])
 
   #prep for ouput files
@@ -49,7 +51,7 @@ def buildTopo(name,file):
       headlabel = item.get('Node Interface')
       taillabel = item.get('Peer Interface')
       #print ( nodeName + " -- "+ peerNodeName +" [taillabel=\"" + taillabel + "\" headlabel=\"" + headlabel +"\"]"  ) #For interface labeling
-      f.write(nodeName + " -- "+ peerNodeName + "\n")
+      f.write("\""+nodeName+"\"" + " -- "+ "\""+peerNodeName+"\"" + "\n")
       if item.get('Peer Type') == "spine":
         spinerank.add(peerNodeName)
       elif item.get('Type') == "l3leaf":
@@ -57,11 +59,11 @@ def buildTopo(name,file):
       allnodes.add(nodeName)
   f.write("\n")
   f.write("subgraph { \n")
-  f.write("{ rank=same;\n")
+  f.write("{ rank=min;\n")
   for item in spinerank:
     f.write(item+"\n")
   f.write("};\n")
-  f.write("{ rank=same;\n")
+  f.write("{ rank=max;\n")
   for item in leafrank:
     f.write(item+"\n")
   f.write("};\n")
@@ -75,9 +77,42 @@ def buildTopo(name,file):
   f.close()
 
 
+def buildDiagram(name,file,exportType):
+  sourceType=name
+  sourceFilename = file
+  outputType = exportType
+  if not os.path.exists('./documentation/diagrams/'):
+    os.makedirs('./documentation/diagrams/')
+  #topotype = ('./documentation/diagrams/' + name + ".dot")
+  #using common output types, reference https://graphviz.org/docs/outputs/
+  if outputType == 'png':
+    #dot -Tpng *-p2p.dot > *-p2p.png
+    os.system('dot -Tpng '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.png' )
+  elif outputType == 'svg':
+    os.system('dot -Tsvg '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.svg' )
+  elif outputType == 'bmp':
+    os.system('dot -Tbmp '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.bmp' )
+  elif outputType == 'gif':
+    os.system('dot -Tgif '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.gif' )
+  elif outputType == 'jpg':
+    os.system('dot -Tjpg '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.jpg' )
+  elif outputType == 'json':
+    os.system('dot -Tjson '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.json' )
+  elif outputType == 'pdf':
+    os.system('dot -Tpdf '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.pdf' )
+  elif outputType == 'webp':
+    os.system('dot -Twebp '+ sourceFilename[0] + ' > ' + './documentation/diagrams/' + sourceType +'.webp' )
+  else:
+    print('Output source not defined, please choose output type')
+
 p2p = find('*-p2p-links.csv', './documentation/fabric/')
 topofile = find('*-topology.csv', './documentation/fabric/')
 print("Building Peer To Peer Topology source file")
 buildTopo('PeerToPeer',p2p)
 print("Building Fabric Topology source file")
 buildTopo('FabricTopology',topofile)
+
+p2pdiag = find('PeerToPeer.dot', './documentation/diagrams/')
+buildDiagram('PeerToPeer',p2pdiag,'png')
+fabricdiag = find('FabricTopology.dot', './documentation/diagrams/')
+buildDiagram('FabricTopology',fabricdiag,'png')
